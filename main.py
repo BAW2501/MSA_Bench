322d7db10dc6d98e6601ceb489d6749283390cd6
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-06-07 19:00:29 +0100

    working right on single file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pymsa import MSA, Entropy, PercentageOfNonGaps, PercentageOfTotallyConservedColumns, Star, SumOfPairs
 from pymsa import PAM250, Blosum62
-from pymsa.util.fasta import read_fasta_file_as_list_of_pairs, print_alignment
+from pymsa.util.fasta import read_fasta_file_as_list_of_pairs
+from pprint import pprint
 
 
 if __name__ == '__main__':
@@ -11,32 +12,14 @@ if __name__ == '__main__':
     
 
     msa = MSA(aligned_sequences, sequences_id)
-    print_alignment(msa)
-
-    # Percentage of non-gaps and totally conserved columns
-    non_gaps = PercentageOfNonGaps(msa)
-    totally_conserved_columns = PercentageOfTotallyConservedColumns(msa)
-
-    percentage = non_gaps.compute()
-    print("Percentage of non-gaps: {0} %".format(percentage))
-
-    conserved = totally_conserved_columns.compute()
-    print("Percentage of totally conserved columns: {0}".format(conserved))
-
-    # Entropy
-    value = Entropy(msa).compute()
-    print("Entropy score: {0}".format(value))
-
-    # Sum of pairs
-    value = SumOfPairs(msa, Blosum62()).compute()
-    print("Sum of Pairs score (Blosum62): {0}".format(value))
-
-    value = SumOfPairs(msa, PAM250()).compute()
-    print("Sum of Pairs score (PAM250): {0}".format(value))
-
-    # Star
-    value = Star(msa, Blosum62()).compute()
-    print("Star score (Blosum62): {0}".format(value))
-
-    value = Star(msa, PAM250()).compute()
-    print("Star score (PAM250): {0}".format(value))
+    
+    stats = {
+        'Entropy': Entropy(msa).compute(),
+        'PercentageOfNonGaps': PercentageOfNonGaps(msa).compute(),
+        'PercentageOfTotallyConservedColumns': PercentageOfTotallyConservedColumns(msa).compute(),
+        'SumOfPairsBlosum62': SumOfPairs(msa, Blosum62()).compute(),
+        'SumOfPairsPAM250': SumOfPairs(msa, PAM250()).compute(),
+        'StarBlosum62': Star(msa, Blosum62()).compute(),
+        'StarPAM250': Star(msa, PAM250()).compute()
+    }
+    pprint(stats)
